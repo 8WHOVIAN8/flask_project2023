643a015d614bb36e5e5b2f22e430a0897fa6cb44
‎이서연(호크마교양대학 호크마교양대학) <lsy629@ewhain.net>
2023-11-15 12:34:39 +0000

    최종 완성본 ㅠㅠ

--- application.py
+++ application.py
@@ -11,10 +11,39 @@ DB = DBhandler()
 @application.route("/")
 def hello():
     return render_template("index.html")
+    #return redirect(url_for('view_list'))
 
 @application.route("/list")
 def view_list():
-    return render_template("list.html")
+    page = request.args.get("page", 0, type=int)
+    per_page=6
+    per_row=3
+    row_count=int(per_page/per_row)
+    start_idx=per_page*page
+    end_idx=per_page*(page+1)
+    
+    data = DB.get_items()
+    item_counts = len(data)
+    data = dict(list(data.items())[start_idx:end_idx])
+    tot_count = len(data)
+    
+    for i in range(row_count):
+        if (i == row_count-1) and (tot_count%per_row != 0):
+            locals()['data_{}'.format(i)] = dict(list(data.items())[i*per_row:])
+        else:
+            locals()['data_{}'.format(i)] = dict(list(data.items())[i*per_row:(i+1)*per_row])
+            
+    
+    return render_template(
+        "list.html",
+        datas=data.items(),
+        row1=locals()['data_0'].items(),
+        row2=locals()['data_1'].items(),
+        limit=per_page,
+        page=page,
+        page_count=int((item_counts/per_page)+1),
+        total=item_counts
+    )
 
 @application.route("/review")
 def view_review():
@@ -53,9 +82,17 @@ def reg_item_submit():
 def login():
     return render_template("login.html")
 
-@application.route("/login_confirm")
-def login_confirm():
-    return render_template("index.html")
+@application.route("/login_confirm", methods=['POST'])
+def login_user():
+    id_=request.form['id']
+    pw=request.form['pw']
+    pw_hash = hashlib.sha256(pw.encode('utf-8')).hexdigest()
+    if DB.find_user(id_, pw_hash):
+        session['id']=id_
+        return redirect(url_for('hello'))
+    else:
+        flash("Wrong ID or PW!")
+        return render_template("login.html")
 
 @application.route("/signup")
 def signup():
@@ -72,9 +109,21 @@ def register_user():
         flash("user id already exist!")
         return render_template("signup.html")
 
-    print(name,addr,phone,category,status)
+    #print(name,addr,phone,category,status)
     #return render_template("reg_item.html")
+    
+@application.route("/logout")
+def logout_user():
+    session.clear()
+    return redirect(url_for('hello'))
 
+
+@application.route("/view_detail/<name>/")
+def view_item_detail(name):
+    print("###name:",name)
+    data = DB.get_item_byname(str(name))
+    print("####data:",data)
+    return render_template("detail.html", name=name, data=data)
 
 if __name__ == "__main__":
     application.run(host='0.0.0.0', debug=True)
